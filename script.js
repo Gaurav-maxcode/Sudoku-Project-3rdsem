fa21e90490dbd4ef714f96e0b2e17c056e0f186d
Gaurav Aryal <gauaryal@gmail.com>
2025-12-12 14:14:03 +0545

    inputable boxes

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const inputBox = document.querySelectorAll(".smallbox");
-console.log(inputBox);
-
-inputBox.addEventListener("click", function (e) {
-  console.log(e.innerHtml());
+let inputNumber = document.getElementById("smallbox");
+inputNumber.addEventListener("input", function () {
+  // Allow only digits
+  this.value = this.value.replace(/[^0-9]/g, "");
+  // Limit to only 1 character
+  if (this.value.length > 1) {
+    this.value = this.value.slice(0, 1);
+  }
 });
